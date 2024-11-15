a36faea36f0dbecc17e4ca57d0f3929ac4919218
Nandagopal <nandagopal.nsb@gmail.com>
2024-11-15 23:32:33 +0530

    Added grid drawing logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,34 @@
 #include <SFML/Graphics.hpp>
-#include "print.cpp"
-int main()
-{
-    Print print;
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
-    window.setFramerateLimit(144);
+#include <iostream>
+#include <vector>
+#define CELL_SIZE 64
+#define COLUMNS 8
+#define ROWS 8
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event(); window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
+void drawRec(sf::RenderWindow *window) {
+  sf::RectangleShape cell_shape(sf::Vector2f(CELL_SIZE - 3, CELL_SIZE - 3));
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      cell_shape.setPosition(CELL_SIZE * i, CELL_SIZE * j);
+      cell_shape.setFillColor(sf::Color(73, 98, 185));
 
-        window.clear();
-        window.display();
+      window->draw(cell_shape);
     }
+  }
+}
+int main() {
+  sf::Vector2i position = sf::Mouse::getPosition();
+  auto window = sf::RenderWindow({510u, 510u}, "memory-match");
+  window.setFramerateLimit(144);
+  sf::RectangleShape rectangle(sf::Vector2f(50, 50));
+  while (window.isOpen()) {
+    for (auto event = sf::Event(); window.pollEvent(event);) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+    window.clear();
+    drawRec(&window);  // PASS BY REFERENCE
+    window.display();
+  }
 }
